d8ce6e39ab02fdc2aa76be085f581be48debd515
rajatpatil <patilrajat805@gmail.com>
2024-01-26 01:44:41 +0530

    initial commit

---
+++ src/main/java/com/rajatngc/searchwithease/SearchWithEaseApplication.java
@@ -0,0 +1,13 @@
+package com.rajatngc.searchwithease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchWithEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchWithEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajatngc/searchwithease/SearchWithEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajatngc.searchwithease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchWithEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
